ed56a35a6a0190a7f555098a7452e93b6eaa696a
AbieFzrn <abiefazran@gmail.com>
2025-07-30 21:11:25 -0700

    Add files via upload
    java pak mujib

---
+++ JavaApplication10/src/mahasiswa/koneksi.java
@@ -0,0 +1,26 @@
+package mahasiswa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection koneksi;
+
+    public static Connection getConnection() throws SQLException {
+        if (koneksi == null || koneksi.isClosed()) {
+            try {
+                String url = "jdbc:mariadb://localhost:3306/pmr_visual"; // ganti sesuai DB kamu
+                String user = "root";
+                String password = "";
+
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Koneksi MariaDB Berhasil!");
+            } catch (SQLException e) {
+                System.err.println("Koneksi Gagal! " + e.getMessage());
+                throw e;
+            }
+        }
+        return koneksi;
+    }
+}
